0c0cf974dae190c967137388c7c02457447fa0bb
Dominika Misiuda <misiuda.d@gmail.com>
2023-04-21 11:14:45 +0200

    init project

---
+++ waiter-app/src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Favorite from './components/Favorite/Favorite';
+import About from './components/About/About';
+import Container from './components/Container/Container';
+import NavBar from './components/NavBar/NavBar';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import List from './components/List/List';
+
+const App = () => {
+  return (
+<main>
+      <NavBar />
+      <Container>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/list/:listId" element={<List />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
+    </main>
+  );
+};
+
+export default App;
---
+++ waiter-app/src/index.js
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.querySelector('#root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
